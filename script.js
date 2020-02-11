e1aab51c00d55e19d4e1ffa24ae89421b4d38de1
KaiM <kai_soering@yahoo.com>
2020-02-11 14:05:04 +0100

    button revisado

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-var email = window.prompt("Please add an email:");
-console.log("Thanks for your email");
+// JavaScript to be added
+
+var button = document.getElementById("button-yellow")
+button.onclick = function (){
+    var hola = window.prompt("User Email")
+alert("Thanks for your email!");
+
+    
+}
